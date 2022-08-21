769b86ed0ff3b034b6457750bd99a11021d3f041
kofi-teddy <agudogot@gmail.com>
2022-08-21 20:18:41 +0000

    react components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React from 'react'
+// import React, { useState } from 'react'
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Dashboard from './components/Dashboard/Dashboard'
+import Preferences from './components/Preferences/Preferences'
+import Login from './components/Login/Login'
+import useToken from "./components/useToken";
+
+
+function setToken(userToken) {
+  sessionStorage.setItem("token", JSON.stringify(userToken));
+}
+
+// function getToken() {
+//   const tokenString = sessionStorage.getItem('token');
+
+//   const userToken = JSON.parse(tokenString);
+//   return userToken && userToken.token
+// }
 
 function App() {
+
+  // const [token, setToken] = useState();
+  // const token = getToken();
+  const { token, setToken } = useToken();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+        <h1>Application</h1>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/dashbaord" element={<Dashboard />} />
+                <Route path="/preferences" element={<Preferences />} />
+            </Routes>
+        </BrowserRouter>
     </div>
   );
 }
